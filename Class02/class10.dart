96c62f08b0d4cac9434efa467a1eac95cc58fda9
Ripon <mhripon012@gmail.com>
2024-11-17 14:45:52 +0600

    class 10 complete

---
+++ Class02/class10.dart
@@ -0,0 +1,29 @@
+import 'classes.dart';
+
+void main() {
+  // welcomeMessage(5, 6);
+
+// var printingSum =  addNumber(5, 5);
+// print(printingSum);
+
+  // Cars obj = new Cars("bmw23");
+  var object = new Car();
+  // print(object.year);
+  // print(Cars.backward());
+  
+  print(object.backward());
+  // print(obj.forward(70));
+}
+
+// welcomeMessage(a, b) {
+//   print("Hello there! I'm dart. 😊");
+// }
+//
+// addNumber(int a, int b) {
+//   return a + b;
+
+class Car extends Cars{
+    backward() {
+    print("I'm changed");
+  }
+}
---
+++ Class02/classes.dart
@@ -0,0 +1,16 @@
+class Cars {
+  String Model = "BMW23";
+  int year = 1998;
+
+  // Cars(String des){
+    // print("This is the des of constructor:${des}");
+    
+  // }
+  forward(int speed) {
+    print("The car is moving forward: ${speed} km/hr");
+  }
+
+    backward() {
+    print("The car is moving backward");
+  }
+}
